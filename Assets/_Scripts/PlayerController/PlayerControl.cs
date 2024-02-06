914bf497f8b35b2a99f59dd19441a537a766573e
andrea-cantatore <andreacantatore01@gmail.com>
2024-02-06 21:51:12 +0100

    move on boats

--- Assets/_Scripts/PlayerController/PlayerControl.cs
+++ Assets/_Scripts/PlayerController/PlayerControl.cs
@@ -1,44 +1,65 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    private bool _isMoving = false;
+    //checks
+    private bool _isMoving, _isOnMovingTarget, _isAlive = true;
+    
+    //movements
     private Vector3 _origPos, _targetPos;
     [SerializeField] private float _timeToMove;
-
+    //movements on logs/boat/ecc..
+    private Vector3 _movingTargetPos;
+    private GameObject _movingTargetGameObject;
+    [SerializeField] private float _moveTowardsBoatMiddle;
+    
     private void Awake()
     {
         transform.position = new Vector3(-10f, 0f, 0f);
     }
     private void Update()
     {
-        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && !_isMoving)
-            StartCoroutine(MovePlayer(Vector3.forward));
+        if (_isAlive)
+        {
+            if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space)) && !_isMoving)
+                StartCoroutine(MovePlayer(Vector3.forward, false));
+            
+            if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && !_isMoving)
+                StartCoroutine(MovePlayer(Vector3.back, false));
+            
+            if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && !_isMoving)
+                StartCoroutine(MovePlayer(Vector3.left, false));
+            
+            if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && !_isMoving)
+                StartCoroutine(MovePlayer(Vector3.right, false));
+            if (_isOnMovingTarget)
+            {
+                _movingTargetPos = SetMovingTargetPos();
+                transform.position = Vector3.MoveTowards(transform.position, _movingTargetPos, _moveTowardsBoatMiddle);
+            }
+        }
         
-        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && !_isMoving)
-            StartCoroutine(MovePlayer(Vector3.back));
-        
-        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && !_isMoving)
-            StartCoroutine(MovePlayer(Vector3.left));
-        
-        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && !_isMoving)
-            StartCoroutine(MovePlayer(Vector3.right));
     }
 
-    private IEnumerator MovePlayer(Vector3 direction)
+    private IEnumerator MovePlayer(Vector3 direction, bool isFallingInRiver)
     {
         _isMoving = true;
-
         float _elapsedTime = 0;
+        if (!isFallingInRiver)
+        {
         _origPos = transform.position;
+        _targetPos = _origPos + direction;
+        
         //making target pos fixed to an int value
-        _targetPos = _origPos + direction;          
         _targetPos.x = Mathf.Round(_targetPos.x);
         _targetPos.y = Mathf.Round(_targetPos.y);
         _targetPos.z = Mathf.Round(_targetPos.z);
+        }
+
         
         //player movement logic
         while (_elapsedTime < _timeToMove)
@@ -52,5 +73,37 @@ public class PlayerControl : MonoBehaviour
         
         _isMoving = false;
     }
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Boat")
+        {
+            _movingTargetGameObject = other.gameObject;
+            _isOnMovingTarget = true;
+        }
+        if (other.gameObject.tag == "River")
+        {
+            Debug.Log("fall in river");
+            //FallingInRiver();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Boat")
+        {
+            _isOnMovingTarget = false;
+        }
+    }
+    private Vector3 SetMovingTargetPos()
+    {
+        return _movingTargetGameObject.transform.position;
+    }
+
+    private void FallingInRiver()
+    {
+        _isAlive = false;
+        _targetPos = new Vector3(transform.position.x, transform.position.y - 0.8f, transform.position.z);
+        StartCoroutine(MovePlayer(Vector3.down, true));
+    }
+
 }
