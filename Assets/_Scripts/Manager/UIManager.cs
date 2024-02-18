2b0444b1fc8d51381491c78a5fc9036f86abfbb6
andrea-cantatore <andreacantatore01@gmail.com>
2024-02-18 21:39:45 +0100

    Update UIManager.cs

--- Assets/_Scripts/Manager/UIManager.cs
+++ Assets/_Scripts/Manager/UIManager.cs
@@ -104,16 +104,9 @@ public class UIManager : MonoBehaviour
     private void RestoreData()
     {
         BinaryDataSaver data = SaveSystem.LoadPlayerData();
-        if (data.maxScore != null || data.coins != null)
-        {
-            _maxScore = data.maxScore;
-            _coins = data.coins;
-        }
-        else
-        {
-            _maxScore = 0;
-            _coins = 0;
-        }
+
+        _maxScore = data.maxScore;
+        _coins = data.coins;
 
     }
     private void GameStart()
