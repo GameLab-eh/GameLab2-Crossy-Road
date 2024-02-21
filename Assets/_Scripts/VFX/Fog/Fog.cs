3033f3d8ba8a3f2414c29882f4e409db4c8a6149
STRANOstudios <friand2021@gmail.com>
2024-02-21 10:52:19 +0100

    Fog

--- Assets/_Scripts/VFX/Fog/Fog.cs
+++ Assets/_Scripts/VFX/Fog/Fog.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fog : MonoBehaviour
 {
     [Header("Variables")]
-    [SerializeField] GameObject _backFog;
     [SerializeField] GameObject _leftFog;
     [SerializeField] GameObject _rigthFog;
 
@@ -13,16 +10,15 @@ public class Fog : MonoBehaviour
 
     private void Start()
     {
-        float width = LevelManager.Instance.ChunckWidth;
-        _backFog.transform.localScale = new(_backFog.transform.localScale.x, _backFog.transform.localScale.y, (width / 10f));
-        _leftFog.transform.position = new(width - (width / 4), _leftFog.transform.position.y, _leftFog.transform.position.z);
-        _rigthFog.transform.position = new(width / 4, _leftFog.transform.position.y, _leftFog.transform.position.z);
+        float width = GameManager.Instance.MapManager.GameRowWidth;
+        _leftFog.transform.position = new(-(width / 2), _leftFog.transform.position.y, _leftFog.transform.position.z);
+        _rigthFog.transform.position = new(width / 2, _leftFog.transform.position.y, _leftFog.transform.position.z);
     }
 
     private void Update()
     {
         float z = _player.position.z;
         _leftFog.transform.position = new(_leftFog.transform.position.x, _leftFog.transform.position.y, z);
-        _rigthFog.transform.position = new(_leftFog.transform.position.x, _rigthFog.transform.position.y, z);
+        _rigthFog.transform.position = new(_rigthFog.transform.position.x, _rigthFog.transform.position.y, z);
     }
 }
