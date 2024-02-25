947426a823b43424a3ed5d5bc465e561488df018
andrea-cantatore <andreacantatore01@gmail.com>
2024-02-25 16:11:53 +0100

    Platoon Animation Scripts

---
+++ Assets/_Scripts/PlatoonScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatoonScript : MonoBehaviour
+{ 
+    private Animator _animator;
+    [SerializeField] private float _minValue,_maxValue;
+
+
+    private void Start()
+    {
+        StartInitializer();
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnReload += StartInitializer;
+    }
+    private void OnDisable()
+    {
+        EventManager.OnReload -= StartInitializer;
+    }
+    private void StartInitializer()
+    {
+        _animator = GetComponent<Animator>();
+        StartCoroutine(AnimationRoutine());
+    }
+
+    private IEnumerator AnimationRoutine()
+    {
+        float index = Random.Range(_minValue, _maxValue);
+        yield return new WaitForSeconds(index);
+        
+        _animator.SetBool("Delay", true);
+    }
+
+}
